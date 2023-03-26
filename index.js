02cffcb1c3006322b3a8245f6261ef6288a89470
Taha <taha.othman@outlook.com>
2023-03-25 22:54:33 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+
+
+function superbowlWin(array){
+
+    let winYear = array.find(item => item.result === "W");
+
+    if(winYear){
+        return winYear.year;
+    }else{
+        return undefined     
+    }
+}   
+    
+
+    
